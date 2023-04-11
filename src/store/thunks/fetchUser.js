1e8e37c7bc0d5a07053969a571237b315ba93d77
Noi-Git <noi.patsin@gmail.com>
2023-04-11 15:37:50 -0400

    make a request to get data to use in reducer

--- src/store/thunks/fetchUser.js
+++ src/store/thunks/fetchUser.js
@@ -1,4 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 // ('users/fetch') --- is create base type --- to discribe the purpose of the thunk
-const fetchUsers = createAsyncThunk('users/fetch')
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get('http://localhost:3005/users')
+
+  return response.data
+})
+
+export { fetchUsers }
