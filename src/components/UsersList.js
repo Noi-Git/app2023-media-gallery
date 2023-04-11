16ada3cbf543feaa3a0dada4950516791fef54e2
Noi-Git <noi.patsin@gmail.com>
2023-04-11 18:01:45 -0400

    use useSelector to show isLoading, data, error

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,9 +1,15 @@
-import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchUsers } from '../store'
 
 const UsersList = () => {
   const dispatch = useDispatch()
+  const { isLoading, data, error } = useSelector((state) => {
+    // return state.users //{data: [], isLoading: false, error: null}
+
+    // we are going to destructure those properties
+    return state.users
+  })
 
   useEffect(() => {
     // run thunk
