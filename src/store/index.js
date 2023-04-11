26e39c5e0a50ed94c2e6b5770e28271f5068c006
Noi-Git <noi.patsin@gmail.com>
2023-04-11 17:47:20 -0400

    export thunk

--- src/store/index.js
+++ src/store/index.js
@@ -5,3 +5,5 @@ import { usersReducer } from './slices/usersSlice'
 // create store
 //usersReducer is the combine reducer from usersSlice
 export const store = configureStore({ reducer: { users: usersReducer } })
+
+export * from './thunks/fetchUser'
