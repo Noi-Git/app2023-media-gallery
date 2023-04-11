ad13e06fa801994ea82c339c761a7a151140edfe
Noi-Git <noi.patsin@gmail.com>
2023-04-11 17:53:33 -0400

    dispatch thunk function fetchUsers

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUsers } from '../store'
 
 const UsersList = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // run thunk
+    dispatch(fetchUsers())
+  }, [dispatch]) // add [dispatch] to sholve react warning
+
   return <div>UsersList</div>
 }
 
