e77c8768e0f85a1eff3474394913452c812d2cd6
Noi-Git <noi.patsin@gmail.com>
2023-04-11 22:02:52 -0400

    create classNames step 1

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,24 +1,16 @@
 import classNames from 'classnames'
 
 const Skeleton = ({ times }) => {
-  /* === first solution
-  const boxes = []
+  const outerClassNames = classNames()
+  const innerClassNames = classNames()
 
-  for (let i = 0; i < times; i++) {
-    boxes.push(<div key={i}></div>)
-  }
-
-  return boxes
-  */
-
-  // this one below is doing the exact same thing with the for...loop
   const boxes = Array(times)
     .fill(0)
     .map((_, i) => {
       // (_, i):  _ mean, we don't care about that first argument; we only need index
       return (
-        <div key={i}>
-          <div />
+        <div key={i} classNames={outerClassNames}>
+          <div className={innerClassNames} />
         </div>
       )
     })
