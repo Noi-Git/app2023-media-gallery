985c16614ad3e8758f00d8203ac6baf3e1aa5900
Noi-Git <noi.patsin@gmail.com>
2023-04-13 18:51:00 -0400

    form a connection with the reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 // export point for everyting related to redux
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { usersReducer } from './slices/usersSlice'
+import { albumsApi } from './apis/albumsApi'
 
 // create store
 //usersReducer is the combine reducer from usersSlice
-export const store = configureStore({ reducer: { users: usersReducer } })
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+})
 
 export * from './thunks/fetchUser'
 export * from './thunks/addUser'
