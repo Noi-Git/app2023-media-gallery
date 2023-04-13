896dbfe0fe665f62e986bd5e3407932cb89f1fb3
Noi-Git <noi.patsin@gmail.com>
2023-04-13 13:15:36 -0400

    move user list content

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -26,13 +26,6 @@ const UsersList = () => {
   } else {
     content = data.map((user) => {
       return <UsersListItem key={user.id} user={user} /> // pass user to UsersListItem via props
-      // return (
-      //   <div key={user.id} className='mb-2 border rounded'>
-      //     <div className='flex p-2 justify-between items-center cursor-pointer'>
-      //       {user.name}
-      //     </div>
-      //   </div>
-      // )
     })
   }
 
