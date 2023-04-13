1b475296367fa2ead76b206cfc01a088d92b479d
Noi-Git <noi.patsin@gmail.com>
2023-04-13 15:26:27 -0400

    add expanded buttons and onClick

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
 
 const ExpandablePanel = ({ header, children }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+
   return (
     <div className='mb-2 border rounded'>
       <div className='flex p-2 justify-between items-center cursor-pointer'>
         <div className='flex flex-row items-center justify-between'>
           {header}
+        </div>
+        <div onClick={handleClick}>
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
         </div>
       </div>
       <div className='p-2 border-t'>{children}</div>
