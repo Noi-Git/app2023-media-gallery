ad9820ff4fa6ff03078dfbaedb978be188c1c5b8
Noi-Git <noi.patsin@gmail.com>
2023-04-13 20:19:09 -0400

    add comment for some explaination

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,9 +2,13 @@ import { useFetchAlbumsQuery } from '../store'
 
 const AlbumsList = ({ user }) => {
   // when calling useFetchAlbumsQuery -- we are fetching data
-  const { data, error, isLoading } = useFetchAlbumsQuery(user)
+  // the (user) argument -- is send along to the query function in albumsApi -- query: (user) => {}
+  // useFetchAlbumsQuery(user) -- is like useEffect
+  // const { data, error, isLoading } = useFetchAlbumsQuery(user)
+  const result = useFetchAlbumsQuery(user)
 
-  console.log(data, error, isLoading)
+  // console.log(data, error, isLoading)
+  console.log('-- result --: ', result)
 
   return <div>Albums for {user.name}</div>
 }
