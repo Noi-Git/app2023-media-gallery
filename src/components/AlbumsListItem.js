327b175e866bfbd1c6ddb3770173616569ee3289
Noi-Git <noi.patsin@gmail.com>
2023-04-14 18:04:01 -0400

    add remove album functionality

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,11 +1,18 @@
 import { GoTrashcan } from 'react-icons/go'
+import { useRemoveAlbumMutation } from '../store'
 import Button from './Button'
 import ExpandablePanel from './ExpandablePanel'
 
 const AlbumsListItem = ({ album }) => {
+  const [removeAlbum, results] = useRemoveAlbumMutation()
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album)
+  }
+
   const header = (
     <div>
-      <Button loading={''}>
+      <Button loading={results.isLoading} onClick={handleRemoveAlbum}>
         <GoTrashcan />
       </Button>
       {album.title}
