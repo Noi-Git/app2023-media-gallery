7c323e0534603039705157788fbe3bd705a39d7a
Noi-Git <noi.patsin@gmail.com>
2023-04-13 20:30:25 -0400

    show album  content

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,16 +1,38 @@
 import { useFetchAlbumsQuery } from '../store'
+import Skeleton from './Skeleton'
+import ExpandablePanel from './ExpandablePanel'
+import Button from './Button'
 
 const AlbumsList = ({ user }) => {
   // when calling useFetchAlbumsQuery -- we are fetching data
   // the (user) argument -- is send along to the query function in albumsApi -- query: (user) => {}
   // useFetchAlbumsQuery(user) -- is like useEffect
-  // const { data, error, isLoading } = useFetchAlbumsQuery(user)
-  const result = useFetchAlbumsQuery(user)
-
+  const { data, error, isLoading } = useFetchAlbumsQuery(user)
   // console.log(data, error, isLoading)
-  console.log('-- result --: ', result)
 
-  return <div>Albums for {user.name}</div>
+  let content
+  if (isLoading) {
+    content = <Skeleton time={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>
+      // header={header} -- is the props passes down from line 20 above
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  )
 }
 
 export default AlbumsList
