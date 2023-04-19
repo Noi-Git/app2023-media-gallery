0b4180581fcce70e859a9ac2a8196c05343399c8
Noi-Git <noi.patsin@gmail.com>
2023-04-19 17:22:43 -0400

    disable button when loading is true

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -4,13 +4,17 @@ import Button from './Button'
 const PhotosList = ({ album }) => {
   useFetchPhotosQuery(album)
 
-  const [addPhoto, addPhtotResult] = useAddPhotoMutation()
+  const [addPhoto, addPhotoResult] = useAddPhotoMutation()
+
+  const handleAddPhoto = () => {}
 
   return (
     <div>
       <div className='m-2 flex flex-row items-center justify-between'>
         <h3 className='text-lg font-bold'>Photos In {album.title} </h3>
-        <Button loading={''}>+ Add Photo</Button>
+        <Button loading={addPhotoResult.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
       </div>
     </div>
   )
