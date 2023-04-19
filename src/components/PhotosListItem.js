8fa7b8b55a23a29148124b883ae39fd4fe01fe9b
Noi-Git <noi.patsin@gmail.com>
2023-04-19 17:46:33 -0400

    show image

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const PhotosListItem = () => {
-  return <div>PhotosListItem</div>
+const PhotosListItem = ({ photo }) => {
+  return (
+    <div>
+      <img
+        src={photo.url}
+        alt={`random pic of ${photo.title}`}
+        className='h-20 w-20'
+      />
+    </div>
+  )
 }
 
 export default PhotosListItem
