db52027badfa0f7a754f1d5df079c9da9f872232
Noi-Git <noi.patsin@gmail.com>
2023-04-19 17:15:37 -0400

    run useAddPhotoMutation hook to get data

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,7 +1,10 @@
-import { useFetchPhotosQuery } from '../store'
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store'
+import Button from './Button'
 
 const PhotosList = ({ album }) => {
   useFetchPhotosQuery(album)
+
+  const [addPhoto, addPhtotResult] = useAddPhotoMutation()
   return 'PhotosList'
 }
 
