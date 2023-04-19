ecd6c3e1fe5ebe536e4ea1439e548030d6aecacb
Noi-Git <noi.patsin@gmail.com>
2023-04-19 17:52:32 -0400

    add delete icon when mouse over the image

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import { GoTrashcan } from 'react-icons/go'
+import { useRemovePhotoMutation } from '../store'
 
 const PhotosListItem = ({ photo }) => {
   return (
-    <div className='relative m-2'>
+    <div className='relative m-2 cursor-pointer'>
       <img
         src={photo.url}
         alt={`random pic of ${photo.title}`}
         className='h-20 w-20'
       />
+      <div className='absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80'>
+        <GoTrashcan className='tex-3xl' />
+      </div>
     </div>
   )
 }
